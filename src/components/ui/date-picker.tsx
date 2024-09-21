9ac1a8575e664e9d3f55f81c670b394ddbfed024
Aditya Mandley <mandleyaditya18@gmail.com>
2024-09-21 19:29:05 +0530

    update date formatting in add expense form

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -28,7 +28,7 @@ export function DatePicker({
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {value ? format(value, "PPP") : <span>Pick a date</span>}
+          {value ? format(value, "MMM do, yyyy") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
